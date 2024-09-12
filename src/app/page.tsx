c7a104696182116e94a86bb82506de578caae725
AlanaDubie <alanadubie922@gmail.com>
2024-09-12 03:40:24 -0700

    Added new ViewPage image and adjusted spacing in Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,20 @@ import Header from '@/components/Header'
 import Intro from '@/components/Intro'
 import Tutorial from '@/components/Tutorial'
 import FAQ from '@/components/FAQ'
-import ViewingPage from './ViewingPage.jpeg'
+import ViewingPage from './ViewingPage.png'
 import Consent from '@/components/Consent'
 
 export default function LandingPage() {
   return (
     <>
       <Header />
-      <div className="mb-0 mt-10 pb-0">
+      <div className="mb-0 mt-20 flex flex-col items-center gap-10 pb-0 ">
         <Intro />
-        <Image src={ViewingPage} alt="Viewing Page" className="mx-auto" />
+        <Image
+          src={ViewingPage}
+          alt="Viewing Page"
+          className="mx-auto h-auto w-[70%]"
+        />
         <Tutorial />
         <FAQ />
       </div>
