a1460e23f2055557822ab2f0db22c31a7980e484
Zhenyu Yu <zhenyuyu@umail.ucsb.edu>
2025-03-24 16:06:31 -0700

    remove console log, added more descriptive test tile in EventForm.test

--- src/components/ViewEvent.tsx
+++ src/components/ViewEvent.tsx
@@ -399,13 +399,15 @@ const ViewEvent = () => {
                         )
                         const userSchedule = user?.timesegments
                         if (userSchedule != null) {
-                          console.log('BeforeEditSchedule', beforeEditSchedule)
-                          console.log('Schedule', schedule)
-                          console.log('UserSchedule', userSchedule)
-                          setBeforeEditSchedule(
-                            JSON.parse(JSON.stringify(userSchedule)),
-                          )
-                          setSchedule(JSON.parse(JSON.stringify(userSchedule)))
+                          try {
+                            const userScheduleCopy = JSON.parse(
+                              JSON.stringify(userSchedule),
+                            )
+                            setBeforeEditSchedule(userScheduleCopy)
+                            setSchedule(userScheduleCopy)
+                          } catch (error) {
+                            console.error('Error parsing user schedule:', error)
+                          }
                         } else {
                           setSchedule({})
                           setBeforeEditSchedule({})
@@ -453,8 +455,6 @@ const ViewEvent = () => {
                   <button
                     className="btn btn-outline rounded-full px-4 py-2 text-red-400 hover:!border-red-400 hover:bg-red-300"
                     onClick={() => {
-                      console.log('BeforeEditSchedule', beforeEditSchedule)
-                      console.log('Schedule', schedule)
                       setSchedule(beforeEditSchedule)
                       setIsButtonsVisible(false)
                       setIsAvailable(false)
